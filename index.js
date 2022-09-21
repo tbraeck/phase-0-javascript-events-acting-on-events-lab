b142bc2ec9799c1c9452a0637ddacf67592959fe
Tate <tatebraeckel@gmail.com>
2022-09-21 17:57:26 -0600

    commit tree 70be35b496b5dce271d3756affae15948f14ac65

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Your code here
+var dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+var dodger = document.getElementById("dodger");
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+        console.log(e, "crampers");
+
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+        console.log(e, "Farty babies");
+    }
+});
+
+
+
+
+
+// document.addEventListener("keydown", function (e) {
+//     if (e.key === "ArrowRight") {
+//         moveDodgerRight();
+//         console.log(e);
+
+//     }
+// });
+
+
